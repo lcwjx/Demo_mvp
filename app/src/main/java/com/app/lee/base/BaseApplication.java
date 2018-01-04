f28aa2dcda9c87fee2a1403bbebc1d17db763b51
李晨 <lichen@newhope.cn>
2018-01-04 11:32:12 +0800

    Commit

---
+++ app/src/main/java/com/app/lee/base/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.app.lee.base;
+
+import android.app.Application;
+
+/**
+ * Created by lichen on 2017/12/26.
+ */
+
+public class BaseApplication extends Application {
+
+
+}
---
+++ app/src/main/java/com/app/lee/base/interfaces/IPresenter.java
@@ -0,0 +1,12 @@
+package com.app.lee.base.interfaces;
+
+/**
+ * Created by lichen on 2017/12/26.
+ */
+
+public interface IPresenter {
+
+    void onStart();
+
+    void onDestroy();
+}
